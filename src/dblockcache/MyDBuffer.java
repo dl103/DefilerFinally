ba807a2834e44a74a4c1fefa220cd9c62c7be645
Yi Hong Poo <poo_yh@hotmail.com>
2014-04-12 22:46:26 -0400

    filled up some methods for dbuffer

--- src/dblockcache/MyDBuffer.java
+++ src/dblockcache/MyDBuffer.java
@@ -1,44 +1,73 @@
 package dblockcache;
 
+import common.Constants;
+
 public class MyDBuffer extends DBuffer {
 
-	private boolean isClean, isValid, isBusy, isPinned, isHeld;
+	private boolean isClean, isValid, isBusy, isPinned, isHeld, isPushing, isFetching;
 	private byte[] myBuffer;
-	
-	
-	
-	
-	
-	
+	private int blockID;
+	private Object validLock, cleanLock;
+	private MyVirtualDisk disk; // or whatever you call it
+
+	public MyDBuffer(){
+		
+		//blockID= //some id
+		//disk = //somedisk
+	}
 	/* Start an asynchronous fetch of associated block from the volume */
-	public void startFetch();
+	public void startFetch(){
+		isFetching=true;
+
+		try {
+			disk.startRequest(this, Constants.DiskOperationType.READ);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} 
+	}
 
 	/* Start an asynchronous write of buffer contents to block on volume */
-	public void startPush();
+	public void startPush(){
+		isPushing=true;
+		
+		try {
+			disk.startRequest(this, Constants.DiskOperationType.WRITE);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} 
+	}
 
 	/* Check whether the buffer has valid data 
 	 A dbuf is valid iff it has the “correct” copy of the data */ 
 	public boolean checkValid(){
 		return isValid;
 	}
-	
+
 	/*Suggestion.  
 	 * A dbuf is pinned if I/O is in progress, i.e., a VDF request has started but not yet completed.  
-	 * A dbuf is held if DFS obtained a reference to the dbuf from getBlock but has not yet released the dbuf.  
 	 * Don’t evict a dbuf that is pinned or held: pick another candidate.
 	 * */
 	public boolean checkPinned(){
 		return isPinned;
 	}
-	
+
+	/*
+	 *  A dbuf is held if DFS obtained a reference to the dbuf from getBlock but has not yet released the dbuf.  
+	 */
 	public boolean checkHeld(){
 		return isHeld;
 	}
 
 	/* Wait until the buffer has valid data, i.e., wait for fetch to complete */
 	public boolean waitValid(){
-		while (true){
-			//
+		while (!isValid && isFetching){
+			try{
+				synchronized(validLock){
+					validLock.wait();
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		return true;
 	}
@@ -53,7 +82,14 @@ public class MyDBuffer extends DBuffer {
 
 	/* Wait until the buffer is clean, i.e., wait until a push operation completes */
 	public boolean waitClean(){
-		while (true){
+		while (isPushing&&!isClean){
+			try{
+				synchronized(cleanLock){
+					cleanLock.wait();
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 
 		}
 		return true;
@@ -61,7 +97,7 @@ public class MyDBuffer extends DBuffer {
 
 	/* Check if buffer is evictable: not evictable if I/O in progress, or buffer is held */
 	public boolean isBusy(){
-		return isHeld || isBusy;
+		return isHeld || isBusy|| isPinned;
 	}
 
 	/*
@@ -70,21 +106,40 @@ public class MyDBuffer extends DBuffer {
 	 * count are for the buffer array, not the DBuffer. Upon an error, it should
 	 * return -1, otherwise return number of bytes read.
 	 */
-	public int read(byte[] buffer, int startOffset, int count);
+	public int read(byte[] buffer, int startOffset, int count){
+		if (!isHeld || !isPinned || !isValid){
+			return -1;
+		}
+		int bytesRead=0;
+		
+		for (int i=0; i<count;i++){
+
+			buffer[i+startOffset]=myBuffer[i];
+			bytesRead++;
+		}
+
+		return bytesRead;
+	}
 
 	/*
 	 * writes into the DBuffer from the contents of buffer[] array. startOffset
 	 * and count are for the buffer array, not the DBuffer. Mark buffer dirty!
 	 * Upon an error, it should return -1, otherwise return number of bytes
-	 * written.
+	 * written.s
 	 */
-	public int write(byte[] buffer, int startOffset, int count);
+	public int write(byte[] buffer, int startOffset, int count){
+		return -1;
+	}
 
 	/* An upcall from VirtualDisk layer to inform the completion of an IO operation */
-	public void ioComplete();
+	public void ioComplete(){
+		//no idea what to do here
+	}
 
 	/* An upcall from VirtualDisk layer to fetch the blockID associated with a startRequest operation */
-	public int getBlockID();
+	public int getBlockID(){
+		return blockID;
+	}
 
 	/* An upcall from VirtualDisk layer to fetch the buffer associated with DBuffer object*/
 	public byte[] getBuffer(){
